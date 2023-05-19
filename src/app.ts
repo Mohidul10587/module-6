e539138f17424f601b2434c04782cf4721349ea8
mohid <mohid10587@gmail.com>
2023-05-19 12:41:53 +0600

    use express.json()

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import userRoute from './app/modules/user/user.route'
 
 const app: Application = express()
 app.use(cors())
-
+app.use(express.json())
 
 app.use('/api/v1/user',userRoute)
 export default app;
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,12 @@
 
 import express, { Application, NextFunction, Request, Response } from 'express';
 import { createUserToDb, getUsersFromDb } from './user.service';
+import { IUser } from './user.interface';
 
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const data = req.body
-    console.log('this is my data', data)
+    console.log('This is my data', data)
     const user = await createUserToDb(data);
     res.status(200).json({
         status: 'success',
@@ -13,11 +14,8 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
     })
 }
 
-export const getUser = async (req: Request, res: Response, next: NextFunction) => {
-    const data = req.body
+export const getUser = async ():Promise<IUser[]> => {
+
     const user = await getUsersFromDb();
-    res.status(200).json({
-        status: 'success',
-        data: user,
-    })
+   return user ;
 }
